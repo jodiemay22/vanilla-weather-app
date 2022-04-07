b90086ad0556ad8aa75b86d52ec782ccdd275e38
jodiemay22 <97054128+jodiemay22@users.noreply.github.com>
2022-04-07 17:49:00 +0100

    added current forecast data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+function displayForecast(response) {
+  let cityElement = document.querySelector("#city-element");
+  let countryElement = document.querySelector("#country-element");
+  let tempElement = document.querySelector("#temp-element");
+  let feelsLikeElement = document.querySelector("#feels-like");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  cityElement.innerHTML = response.data.name;
+  countryElement.innerHTML = response.data.sys.country;
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windSpeedElement.innerHTML = response.data.wind.speed;
+}
+let city = "London";
+let apiKey = "120813c35ed58ddba478b72f03dd1a3e";
+let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
+axios.get(apiURL).then(displayForecast);
