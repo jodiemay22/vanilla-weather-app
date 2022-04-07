b894efa17236c225383f80c332564ad76c8e3551
jodiemay22 <97054128+jodiemay22@users.noreply.github.com>
2022-04-07 19:24:11 +0100

    Added weather icon from API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayForecast(response) {
   let cityElement = document.querySelector("#city-element");
   let countryElement = document.querySelector("#country-element");
@@ -5,15 +22,21 @@ function displayForecast(response) {
   let feelsLikeElement = document.querySelector("#feels-like");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#current-date");
+  let iconElement = document.querySelector("#icon");
   cityElement.innerHTML = response.data.name;
   countryElement.innerHTML = response.data.sys.country;
   tempElement.innerHTML = Math.round(response.data.main.temp);
   feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
   humidityElement.innerHTML = response.data.main.humidity;
   windSpeedElement.innerHTML = response.data.wind.speed;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 let city = "London";
 let apiKey = "120813c35ed58ddba478b72f03dd1a3e";
 let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-axios.get(apiURL).then(displayForecast);
+http: axios.get(apiURL).then(displayForecast);
